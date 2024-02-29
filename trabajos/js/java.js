897234f3adf636a8635f5cdb8d5f9f1c993ba74a
Alondra <=>
2024-02-29 13:49:04 -0600

    inicializando repositorio

---
+++ trabajos/js/java.js
@@ -0,0 +1,41 @@
+/*let result = 0.1 + 0.2;
+ console.log(result);
+ //tarea investigar porque pasa un numero con muchos decimales 
+
+ //for 
+for(let index =0; index < 11 ; index ++){
+    let number = 10  * index ;
+    console.log(number);
+}
+*/
+
+ //while
+/**let indexWhile = 0 ;
+while(indexWhile < 11){
+    let number =  indexWhile * 10 ;
+    console.log(number);
+    indexWhile ++;
+}
+*/
+ //do-while
+
+// let indexDoWhile = 0;
+ //do{
+   // let number =  indexDoWhile * 10 ;
+   // console.log(number);
+   // indexDoWhile ++;
+ //} while(indexDoWhile < 10);
+
+//tablaMultiplicarFor(8,100);
+function tablaMultiplicarFor(){
+    for(let index =0; index < 11 ; index <=100){
+        let number = 10  * index ;
+        console.log(tablaMultiplicarFor(8,100))
+    }
+}
+
+
+//tablaMultiplicarWhile(8,10);
+
+
+//tablaMultiplicarDoWhile(7,5);
